596e1e08e7c9a981dd4f3b8ae4881223f42a457c
Mohit Muthanna <mohit@muthanna.com>
2010-04-15 02:02:13 -0400

    Added repo updating crontab script.
    Also added documentation for using the crontab/ directory.

---
+++ cron/update_local_repositories.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script updates all local repositories by running "git fetch" on
+# them. Ideally, this is run with the git shell-prompt customizations
+# in ~/s/dotfiles/scripts/prompt.sh such that the user is made aware of
+# new data in a remote repo.
+#
+# Add this script to your crontab. For example:
+#
+# 0,15,30,45 * * * * ~/s/cron/update_local_repositories.sh >/dev/null 2>&1
+
+for dir in w p s x
+do
+  spath=~/$dir
+  if [ -d $spath ]
+  then
+    echo "Updating $spath..."
+    cd $spath
+    git fetch
+  fi
+done
