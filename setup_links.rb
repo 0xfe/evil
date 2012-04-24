8b863c7af0de0189748e9962cf4953f81d729ff6
Mohit Cheppudira <mohit@muthanna.com>
2012-04-24 18:37:13 -0400

    Updated sublime mac prefs.

--- setup_links.rb
+++ setup_links.rb
@@ -35,7 +35,7 @@ ln_sf("#{EVIL_HOME}/dotfiles/brackup.conf", "#{TARGET}/.brackup.conf")
 if is_mac?
   ln_sf("/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl",
         "#{EVIL_HOME}/bin/subl") if is_mac?
-  ln_sf("#{EVIL_HOME}/dotfiles/sublime/mac".
+  ln_sf("#{EVIL_HOME}/dotfiles/sublime/mac",
         "#{TARGET}/Library/Application Support/Sublime Text 2/Packages/User")
 else
   ln_sf("#{EVIL_HOME}/dotfiles/sublime/linux",
