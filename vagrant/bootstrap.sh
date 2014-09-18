d98b1bd56fe1c6568375fa75c14233d4cef5130d
Mohit Cheppudira <mohit@muthanna.com>
2014-09-18 13:52:53 +0000

    Updated vagrant scripts.

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,32 @@
+# Bootstrap Notespace
+
+USER=mohit
+HOME=/home/$USER
+
+apt-get update
+
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password foobar'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password foobar'
+apt-get -y install mysql-server
+
+apt-get install -y \
+  build-essential sqlite3 libsqlite3-dev mysql-server \
+  libmysqlclient-dev libxml2-dev libxslt1-dev zlib1g-dev node \
+  libssl-dev lighttpd apache2-utils git-core
+
+# For RVM
+apt-get install -y \
+  libreadline6-dev libyaml-dev autoconf \
+  automake libtool bison pkg-config libffi-dev
+
+# curl -L https://get.rvm.io | bash -s stable --ruby
+# source /usr/local/rvm/scripts/rvm
+# rvm install 1.9.3
+
+# For AWS
+apt-get install -y openjdk-7-jre-headless
+
+groupadd $USER
+useradd -M -d $HOME -g $USER -G sudo,www-data -s /bin/bash $USER
+chown $USER:$USER $HOME
+mount -t vboxsf -o uid=`id -u $USER`,gid=`id -g $USER` home_$USER $HOME
